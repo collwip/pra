a10fe3ede15f9d27f5bc00aff09f74ef2ed9a531
collwip <volohov3152@mail.ru>
2024-06-17 13:03:37 +0300

    first

---
+++ ChatSP/Login.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatSP
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        MainWindow.main.Content = new Registratio();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+
+
+            if (Log.Text == "")
+            {
+                MessageBox.Show("Введите ваш логин");
+            }
+            else if (Pass.Password == "")
+            {
+                MessageBox.Show("Введите ваш пароль");
+            }
+            else if (Roless.Text == "")
+            {
+                MessageBox.Show("Введите ваш ID");
+            }
+
+
+
+            string LoginUser = Log.Text;
+            string PasswordUser = Pass.Password;
+           /// int Roles = Convert.ToInt32(Roless.Text);
+            
+
+                var Userin = AppConnect.modelOdb.User.FirstOrDefault(x => x.Login == LoginUser && x.Password == PasswordUser);
+
+                    if (Userin == null)
+                    {
+                        MessageBox.Show("Вы ввели что-то неправильно", "Erro", MessageBoxButton.OK);
+                    } 
+                    else
+                    {
+                        MessageBox.Show("Вы ввели ВСЁ правильно", "Entrance", MessageBoxButton.OK);
+
+                        MainWindow.main.Content = new MainPage1();
+                        
+
+                       
+                    }
+                
+               
+
+
+            
+
+           
+            
+        }
+    }
+}
---
+++ ChatSP/Registratio.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatSP
+{
+    /// <summary>
+    /// Логика взаимодействия для Registratio.xaml
+    /// </summary>
+    public partial class Registratio : Page
+    {
+        public Registratio()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Add(object sender, RoutedEventArgs e)
+        {
+
+            if (Log.Text == "")
+            {
+                MessageBox.Show("Укажите ваш будущий логин");
+            }
+
+            else if (Pass.Password == "")
+            {
+                MessageBox.Show("Укажите ваш пароль");
+            }
+
+            else
+            {
+                using (ChatEntities db = new ChatEntities())
+                {
+                    User users= new User();
+                    
+                    users.Login = Log.Text;
+                    users.Password = Pass.Password;
+
+
+                    db.User.Add(users);
+                    db.SaveChanges();
+                }
+            }
+
+
+
+          /*  User user = new User();
+            user.Lgin = Tname.Text;
+            user.Password = Passwords.Password;*/
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.main.Content = new Login();
+        }
+
+        private void Tname_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
